1356b0b777a98364399a6015d71c3f6ca5688778
Ubuntu <ubuntu@ip-172-31-13-146.ap-south-1.compute.internal>
2026-05-01 02:59:54 +0000

    update app

--- myapp/app.py
+++ myapp/app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return """
-    <h1>DevOps CI/CD Pipeline 🚀</h1>
+    <h1>Welcome to DevOps CI/CD Project 🚀</h1>
+    <h2>This is Experiment 3 using GitHub Actions!</h2>
     <p>Deployed using GitHub Actions</p>
-    <p>Status: Running ✅</p>
+    <p>Status: Application is running successfully ✅</p>
     """
 
 if __name__ == "__main__":
